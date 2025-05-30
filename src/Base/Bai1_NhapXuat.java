cbd57220534ed898c0342bf94e084250d22b5f76
nguyenvh60 <contact.tkl.fco@gmail.com>
2025-05-30 11:06:45 +0700

    So sánh

--- src/Base/Bai1_NhapXuat.java
+++ src/Base/Bai1_NhapXuat.java
@@ -4,4 +4,9 @@ public class Bai1_NhapXuat {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+
+    // Mỗi dòng mã chạy trong Java phải nằm trong dấu class. Và tên lớp phải luôn bắt đầu bằng chữ cái đầu tiên viết hoa
+    // Tên của tệp java phải khớp với tên lớp. Khi lưu tệp, hãy lưu tệp bằng tên lớp và thêm ".java" vào cuối tên tệp
+
 }
---
+++ src/Base/Bai8_ToanTuSoSanh.java
@@ -0,0 +1,22 @@
+package Base;
+
+public class Bai8_ToanTuSoSanh {
+    public static void main(String[] args) {
+        //Toán tử so sánh được sử dụng để so sánh hai giá trị (hoặc biến).
+        //  giúp chúng ta tìm ra câu trả lời và đưa ra quyết định.
+
+        //Giá trị trả về của phép so sánh là hoặc true. false Những giá trị này được gọi là giá trị Boolean
+
+        int x = 5;
+        int y = 3;
+        System.out.println(x > y); // trả về true vì 5>3 , ngược lại là false
+
+
+        // 	Toán tử so sánh != để so sánh xem 2 giá trị có khác nhau không, nếu khác (không bằng) là true
+        int x1 = 5;
+        int y2 = 3;
+        System.out.println(x1!= y2); // trả về true vì 5 không bằng 3
+
+        // Một số phép so sánh khác ( > , < , >= , <= )
+    }
+}
