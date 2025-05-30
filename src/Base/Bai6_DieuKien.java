2091ffb4ba8590bc40564dcb698010d894edfdc6
nguyenvh60 <contact.tkl.fco@gmail.com>
2025-05-30 10:39:28 +0700

    Inital Commit

---
+++ src/Base/Bai6_DieuKien.java
@@ -0,0 +1,14 @@
+package Base;
+
+
+
+public class Bai6_DieuKien {
+    public static void main(String[] args) {
+        int tuoi = 19 ;
+        if (tuoi > 18) {
+            System.out.println("Lớn rồi");
+        }else{
+            System.out.println("Còn nhỏ");
+        }
+    }
+}
